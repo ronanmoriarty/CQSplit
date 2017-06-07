dde93158a5629ecde6fdbb96df4def46736cca12
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-06-07 16:54:23 +0100

    Throw more detailed exception if CommandHandlerDictionary receives two things expecting to handle the same command

--- Cafe.Domain/CommandHandlerDictionary.cs
+++ Cafe.Domain/CommandHandlerDictionary.cs
@@ -17,6 +17,11 @@ namespace Cafe.Domain
                     if (interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == typeof(ICommandHandler<>))
                     {
                         var commandType = interfaceType.GenericTypeArguments[0];
+                        if (_commandHandlerMappings.ContainsKey(commandType))
+                        {
+                            throw new ArgumentException($"More than one handler found for {commandType.FullName}");
+                        }
+
                         _commandHandlerMappings.Add(commandType, commandHandler);
                     }
                 }
