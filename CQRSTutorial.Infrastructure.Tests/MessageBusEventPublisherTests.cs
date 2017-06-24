42ca944d91529770334be724bd2b8215f615730e
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-06-24 02:32:37 +0100

    Quick proof-of-concept after installing RabbitMQ (will tidy up shortly now that I know this approach will work)

---
+++ CQRSTutorial.Infrastructure.Tests/MessageBusEventPublisherTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using CQRSTutorial.Core;
+using NUnit.Framework;
+
+namespace CQRSTutorial.Infrastructure.Tests
+{
+    [TestFixture]
+    public class MessageBusEventPublisherTests
+    {
+        [Test]
+        public void Test()
+        {
+            var messageBusEventPublisher = new MessageBusEventPublisher();
+            messageBusEventPublisher.Publish(new [] {new TestEvent()});
+            Thread.Sleep(1000);
+            messageBusEventPublisher.Stop();
+        }
+
+        public class TestEvent : IEvent
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
---
+++ CQRSTutorial.Infrastructure/MessageBusEventPublisher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using CQRSTutorial.Core;
+using MassTransit;
+
+namespace CQRSTutorial.Infrastructure
+{
+    public class MessageBusEventPublisher : IEventPublisher
+    {
+        private readonly IBusControl _bus;
+
+        public MessageBusEventPublisher()
+        {
+            _bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                sbc.ReceiveEndpoint(host, "test_queue", ep =>
+                {
+                    ep.Handler<IEvent> (context =>
+                    {
+                        var messages = string.Join("\n", context.Message);
+                        return Console.Out.WriteLineAsync($"Received: {messages}");
+                    });
+                });
+            });
+
+            _bus.Start();
+        }
+
+        public void Publish(IEnumerable<IEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                _bus.Publish(@event);
+            }
+        }
+
+        public void Stop()
+        {
+            _bus.Stop();
+        }
+    }
+}
