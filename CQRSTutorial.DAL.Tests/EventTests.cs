240851d93ce47d2cdce90caf734f490f31924876
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-06-28 16:05:01 +0100

    Add test to check we can insert and retrieve FoodOrdered events

--- CQRSTutorial.DAL.Tests/EventTests.cs
+++ CQRSTutorial.DAL.Tests/EventTests.cs
@@ -1,4 +1,7 @@
-﻿using Cafe.Domain.Events;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cafe.Domain;
+using Cafe.Domain.Events;
 using NUnit.Framework;
 
 namespace CQRSTutorial.DAL.Tests
@@ -7,8 +10,6 @@ namespace CQRSTutorial.DAL.Tests
     public class EventTests
     {
         private IEventRepository _eventRepository;
-        private const string Waiter = "John";
-        private const int TableNumber = 123;
 
         [SetUp]
         public void SetUp()
@@ -17,12 +18,15 @@ namespace CQRSTutorial.DAL.Tests
         }
 
         [Test]
-        public void InsertAndRead()
+        public void InsertAndReadTabOpened()
         {
+            const string waiter = "John";
+            const int tableNumber = 123;
+
             var tabOpened = new TabOpened
             {
-                TableNumber = TableNumber,
-                Waiter = Waiter
+                TableNumber = tableNumber,
+                Waiter = waiter
             };
 
             _eventRepository.Add(tabOpened);
@@ -32,8 +36,44 @@ namespace CQRSTutorial.DAL.Tests
             Assert.That(retrievedEvent is TabOpened);
             var retrievedTabOpenedEvent = (TabOpened) retrievedEvent;
             Assert.That(retrievedTabOpenedEvent.Id, Is.EqualTo(tabOpened.Id));
-            Assert.That(retrievedTabOpenedEvent.TableNumber, Is.EqualTo(TableNumber));
-            Assert.That(retrievedTabOpenedEvent.Waiter, Is.EqualTo(Waiter));
+            Assert.That(retrievedTabOpenedEvent.TableNumber, Is.EqualTo(tableNumber));
+            Assert.That(retrievedTabOpenedEvent.Waiter, Is.EqualTo(waiter));
+        }
+
+        [Test]
+        public void InsertAndReadFoodOrdered()
+        {
+            const string foodDescription = "Chicken Madras";
+            const int foodMenuNumber = 234;
+            const decimal foodPrice = 10.5m;
+
+            var foodOrdered = new FoodOrdered
+            {
+                Items = new List<OrderedItem>
+                {
+                    new OrderedItem
+                    {
+                        Description = foodDescription,
+                        IsDrink = false,
+                        MenuNumber = foodMenuNumber,
+                        Price = foodPrice
+                    }
+                }
+            };
+
+            _eventRepository.Add(foodOrdered);
+
+            var retrievedEvent = _eventRepository.Read(foodOrdered.Id);
+
+            Assert.That(retrievedEvent is FoodOrdered);
+            var retrievedFoodOrderedEvent = (FoodOrdered)retrievedEvent;
+            Assert.That(retrievedFoodOrderedEvent.Id, Is.EqualTo(foodOrdered.Id));
+            Assert.That(retrievedFoodOrderedEvent.Items.Count, Is.EqualTo(1));
+            var foodOrderedItem = retrievedFoodOrderedEvent.Items.Single();
+            Assert.That(foodOrderedItem.Description, Is.EqualTo(foodDescription));
+            Assert.That(foodOrderedItem.IsDrink, Is.False);
+            Assert.That(foodOrderedItem.MenuNumber, Is.EqualTo(foodMenuNumber));
+            Assert.That(foodOrderedItem.Price, Is.EqualTo(foodPrice));
         }
     }
 }
