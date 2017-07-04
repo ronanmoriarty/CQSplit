eb9de2bc59837bdd6a4560a230b27dc3be31f6cb
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-07-04 14:31:13 +0100

    Extract SqlExecutor

--- CQRSTutorial.DAL.Tests/OutboxEventPublisherTests.cs
+++ CQRSTutorial.DAL.Tests/OutboxEventPublisherTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 using Cafe.Domain.Events;
 using NUnit.Framework;
 
@@ -13,10 +11,12 @@ namespace CQRSTutorial.DAL.Tests
         private int _tableNumber = 123;
         private string _waiter = "John";
         private TabOpened _tabOpened;
+        private SqlExecutor _sqlExecutor;
 
         [SetUp]
         public void SetUp()
         {
+            _sqlExecutor = new SqlExecutor();
             _outboxEventPublisher = new OutboxEventPublisher(SessionFactory.WriteInstance, new EventRepository(SessionFactory.ReadInstance, IsolationLevel.ReadCommitted));
         }
 
@@ -39,40 +39,14 @@ namespace CQRSTutorial.DAL.Tests
 
         private void AssertThatEventSaved()
         {
-            var numberOfEventsInserted = ExecuteScalar($"SELECT COUNT(*) FROM dbo.Events WHERE Id = '{_tabOpened.Id}'");
+            var numberOfEventsInserted = _sqlExecutor.ExecuteScalar($"SELECT COUNT(*) FROM dbo.Events WHERE Id = '{_tabOpened.Id}'");
             Assert.That(numberOfEventsInserted, Is.EqualTo(1));
         }
 
         [TearDown]
         public void TearDown()
         {
-            ExecuteNonQuery($"DELETE FROM dbo.Events WHERE Id = '{_tabOpened.Id}'");
-        }
-
-        private int ExecuteScalar(string commandText)
-        {
-            using (var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["CQRSTutorial"].ConnectionString))
-            {
-                sqlConnection.Open();
-                using (var command = sqlConnection.CreateCommand())
-                {
-                    command.CommandText = commandText;
-                    return (int) command.ExecuteScalar();
-                }
-            }
-        }
-
-        private void ExecuteNonQuery(string commandText)
-        {
-            using (var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["CQRSTutorial"].ConnectionString))
-            {
-                sqlConnection.Open();
-                using (var command = sqlConnection.CreateCommand())
-                {
-                    command.CommandText = commandText;
-                    command.ExecuteNonQuery();
-                }
-            }
+            _sqlExecutor.ExecuteNonQuery($"DELETE FROM dbo.Events WHERE Id = '{_tabOpened.Id}'");
         }
     }
 }
---
+++ CQRSTutorial.DAL.Tests/SqlExecutor.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CQRSTutorial.DAL.Tests
+{
+    public class SqlExecutor
+    {
+        public int ExecuteScalar(string commandText)
+        {
+            using (var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["CQRSTutorial"].ConnectionString))
+            {
+                sqlConnection.Open();
+                using (var command = sqlConnection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    return (int)command.ExecuteScalar();
+                }
+            }
+        }
+
+        public void ExecuteNonQuery(string commandText)
+        {
+            using (var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["CQRSTutorial"].ConnectionString))
+            {
+                sqlConnection.Open();
+                using (var command = sqlConnection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
