da62a0abee41f2de649a9b14c32faa1170e5808b
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-07-05 15:21:15 +0100

    Introduce ITabEvent so that all events related to Tab aggregate must have TabId

--- Cafe.Domain/Events/FoodServed.cs
+++ Cafe.Domain/Events/FoodServed.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
-using CQRSTutorial.Core;
 
 namespace Cafe.Domain.Events
 {
-    public class FoodServed : IEvent
+    public class FoodServed : ITabEvent
     {
         public int Id { get; set; }
         public int TabId { get; set; }
---
+++ Cafe.Domain/Events/ITabEvent.cs
@@ -0,0 +1,9 @@
+﻿using CQRSTutorial.Core;
+
+namespace Cafe.Domain.Events
+{
+    public interface ITabEvent : IEvent
+    {
+        int TabId { get; set; }
+    }
+}
