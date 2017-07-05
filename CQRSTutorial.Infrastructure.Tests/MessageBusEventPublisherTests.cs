09551f515f19561e6d07195c6dd5a4620b46e71d
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-07-05 09:43:47 +0100

    Add comments to explain purpose of test

--- CQRSTutorial.Infrastructure.Tests/MessageBusEventPublisherTests.cs
+++ CQRSTutorial.Infrastructure.Tests/MessageBusEventPublisherTests.cs
@@ -8,7 +8,7 @@ namespace CQRSTutorial.Infrastructure.Tests
     [TestFixture]
     public class MessageBusEventPublisherTests
     {
-        [Test, Explicit]
+        [Test, Explicit("Not a real automated test - just a quick way to verify if RabbitMQ is up and running on this environment.")]
         public void Can_publish_to_message_queue()
         {
             var messageBusEventPublisher = new MessageBusEventPublisher();
