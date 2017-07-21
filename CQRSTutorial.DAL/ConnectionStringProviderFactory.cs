43e896765534d216d9ca56f50ef442688d9d15f1
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-07-22 00:31:19 +0100

    Introduce ConnectionStringOverride which takes connection string from environment variable instead of app.config

--- CQRSTutorial.DAL/ConnectionStringProviderFactory.cs
+++ CQRSTutorial.DAL/ConnectionStringProviderFactory.cs
@@ -1,9 +1,17 @@
-﻿namespace CQRSTutorial.DAL
+﻿using System;
+
+namespace CQRSTutorial.DAL
 {
     public class ConnectionStringProviderFactory : IConnectionStringProviderFactory
     {
         public IConnectionStringProvider GetConnectionStringProvider()
         {
+            var connectionString = Environment.GetEnvironmentVariable("CQRS_CONNECTIONSTRING_OVERRIDE", EnvironmentVariableTarget.Machine);
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                return new ConnectionStringOverride(connectionString);
+            }
+
             return new AppConfigConnectionStringProvider("CQRSTutorial");
         }
     }
