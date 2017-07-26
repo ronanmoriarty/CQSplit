95531366061e5c1afaa5ca1518340ef322121fdf
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-07-26 11:54:50 +0100

    Create TabController

---
+++ Cafe.Waiter.Web/Controllers/TabController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Cafe.Waiter.Web.Controllers
+{
+    public class TabController : Controller
+    {
+        // GET: Tab
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return Redirect("Index");
+        }
+    }
+}
