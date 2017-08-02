aad831263ee7952433e13f4f454590bc80bbfac7
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-02 17:07:23 +0100

    Add application-started message so that we can manually test file logging shortly

--- Cafe.Waiter.Web/Global.asax.cs
+++ Cafe.Waiter.Web/Global.asax.cs
@@ -1,15 +1,19 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 using log4net.Config;
 
 namespace Cafe.Waiter.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(MvcApplication));
+
         protected void Application_Start()
         {
             XmlConfigurator.Configure();
+            _logger.Info("Cafe.Waiter.Web application started");
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
