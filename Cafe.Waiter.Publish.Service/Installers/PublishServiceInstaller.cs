9740410a54e47b54c1a86418f719964d3aa8bda0
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-08-03 00:04:03 +0100

    Register PublishService and dependencies

--- Cafe.Waiter.Publish.Service/Installers/PublishServiceInstaller.cs
+++ Cafe.Waiter.Publish.Service/Installers/PublishServiceInstaller.cs
@@ -1,6 +1,10 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using System;
+using Cafe.Waiter.DAL;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using CQRSTutorial.DAL;
+using CQRSTutorial.Publisher;
 
 namespace Cafe.Waiter.Publish.Service.Installers
 {
@@ -8,7 +12,24 @@ namespace Cafe.Waiter.Publish.Service.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            container.Register(Classes.FromThisAssembly());
+            const string publishQueuedMessages = "publishQueuedMessages";
+            container.Register(
+                Classes
+                    .FromAssemblyContaining<PublishService>()
+                    .Where(Component.IsInSameNamespaceAs<PublishService>())
+                    .Unless(type => type == typeof(PublishService))
+                    .WithService
+                        .Self()
+                        .WithServiceDefaultInterfaces()
+                    .LifestyleTransient(),
+                Component.For<IConnectionStringProviderFactory>()
+                    .Instance(WriteModelConnectionStringProviderFactory.Instance),
+                Component.For<Action>()
+                    .Instance(() => container.Resolve<OutboxToMessageQueuePublisher>().PublishQueuedMessages())
+                    .Named(publishQueuedMessages),
+                Component.For<PublishService>()
+                    .DependsOn(Dependency.OnComponent("onNewEventQueuedForPublishing", publishQueuedMessages))
+            );
         }
     }
 }
