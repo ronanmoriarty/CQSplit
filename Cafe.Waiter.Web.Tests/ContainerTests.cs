d9aab2cd93e38dff17dd9d1c4b69c30dfd2ab619
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-04 14:12:27 +0100

    Remove redundant test (as long as TabConttroller can be created, that's the main thing)

--- Cafe.Waiter.Web.Tests/ContainerTests.cs
+++ Cafe.Waiter.Web.Tests/ContainerTests.cs
@@ -7,14 +7,6 @@ namespace Cafe.Waiter.Web.Tests
     public class ContainerTests
     {
         [Test]
-        public void Can_instantiate_MessageBus()
-        {
-            var messageBus = Container.Instance.Resolve<IMessageBus>();
-
-            Assert.That(messageBus, Is.Not.Null);
-        }
-
-        [Test]
         public void Can_instantiate_TabController()
         {
             var tabController = Container.Instance.Resolve<Controllers.TabController>();
