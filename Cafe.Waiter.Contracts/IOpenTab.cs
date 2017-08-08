f6590d9839cfe36b626e281a41a13fb64aabb614
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-08 08:43:44 +0100

    Extract IOpenTab interface

---
+++ Cafe.Waiter.Contracts/IOpenTab.cs
@@ -0,0 +1,10 @@
+﻿using CQRSTutorial.Core;
+
+namespace Cafe.Waiter.Contracts
+{
+    public interface IOpenTab : ICommand
+    {
+        int TableNumber { get; set; }
+        string Waiter { get; set; }
+    }
+}
