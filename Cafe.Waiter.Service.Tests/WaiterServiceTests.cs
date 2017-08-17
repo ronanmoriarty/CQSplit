057ac33fcbbd1ea8cda69f60c4fea53f3fc03bc1
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-17 13:56:03 +0100

    Add WaiterServiceTests to ensure bus started when service started

---
+++ Cafe.Waiter.Service.Tests/WaiterServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using CQRSTutorial.Infrastructure;
+using MassTransit;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Cafe.Waiter.Service.Tests
+{
+    [TestFixture]
+    public class WaiterServiceTests
+    {
+        private WaiterService _waiterService;
+        private IMessageBusFactory _messageBusFactory;
+        private IBusControl _busControl;
+
+        [Test]
+        public void Starting_waiter_service_starts_the_bus()
+        {
+            _busControl = Substitute.For<IBusControl>();
+            _messageBusFactory = Substitute.For<IMessageBusFactory>();
+            _messageBusFactory.Create().Returns(_busControl);
+            _waiterService = new WaiterService(_messageBusFactory);
+
+            _waiterService.Start();
+
+            _busControl.Received(1).Start();
+        }
+    }
+}
