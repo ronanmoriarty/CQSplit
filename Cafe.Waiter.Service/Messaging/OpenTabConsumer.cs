8295418688e0c62f5b24df7fb0db58d3ccd12cc5
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-08-21 21:25:22 +0100

    Extract new Consumer base class

--- Cafe.Waiter.Service/Messaging/OpenTabConsumer.cs
+++ Cafe.Waiter.Service/Messaging/OpenTabConsumer.cs
@@ -1,30 +1,13 @@
-using System;
-using System.Threading.Tasks;
 using Cafe.Waiter.Contracts;
 using CQRSTutorial.Core;
-using log4net;
-using MassTransit;
 
 namespace Cafe.Waiter.Service.Messaging
 {
-    public class OpenTabConsumer : IConsumer<IOpenTab>
+    public class OpenTabConsumer : Consumer<IOpenTab>
     {
-        private readonly ICommandDispatcher _commandDispatcher;
-        private readonly ILog _logger = LogManager.GetLogger(typeof(OpenTabConsumer));
-
         public OpenTabConsumer(ICommandDispatcher commandDispatcher)
+            : base(commandDispatcher)
         {
-            _commandDispatcher = commandDispatcher;
-        }
-
-        public async Task Consume(ConsumeContext<IOpenTab> context)
-        {
-            var text = $"Received command: Type: {typeof(IOpenTab).Name}; Command Id: {context.Message.Id}; Aggregate Id: {context.Message.AggregateId}";
-            await Console.Out.WriteLineAsync(text);
-            _logger.Debug(text);
-
-            // TODO: can't see how to instantiate a ConsumeContext for unit testing. Will continue to spike this for now.
-            _commandDispatcher.Dispatch(context.Message);
         }
     }
 }
