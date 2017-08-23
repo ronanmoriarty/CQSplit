cee1082f28fcbdaa6f53c5c529948725e845a10c
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-23 17:15:52 +0100

    Add empty Cafe.Waiter.Query.Service

---
+++ Cafe.Waiter.Query.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Waiter.Query.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
