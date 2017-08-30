488bdb2f870644d0dc4db80846f510e4743d970f
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-08-30 16:42:52 +0100

    Move GetAll functionality to RepositoryBase

--- CQRSTutorial.DAL/RepositoryBase.cs
+++ CQRSTutorial.DAL/RepositoryBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NHibernate;
 
 namespace CQRSTutorial.DAL
@@ -26,6 +27,17 @@ namespace CQRSTutorial.DAL
                 using (session.BeginTransaction())
                 {
                     return session.Get<TTypeToPersist>(id);
+                }
+            }
+        }
+
+        protected IEnumerable<TTypeToPersist> GetAll()
+        {
+            using (var session = SessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    return session.QueryOver<TTypeToPersist>().List();
                 }
             }
         }
--- Cafe.Waiter.DAL/OpenTabsRepository.cs
+++ Cafe.Waiter.DAL/OpenTabsRepository.cs
@@ -6,25 +6,16 @@ using NHibernate;
 
 namespace Cafe.Waiter.DAL
 {
-    public class OpenTabsRepository : RepositoryBase<OpenTab>, IOpenTabsRepository
+    public class OpenTabsRepository : RepositoryBase<Serialized.OpenTab>, IOpenTabsRepository
     {
-        private readonly ISessionFactory _sessionFactory;
-
         public OpenTabsRepository(ISessionFactory sessionFactory)
             : base(sessionFactory)
         {
-            _sessionFactory = sessionFactory;
         }
 
         public IEnumerable<OpenTab> GetOpenTabs()
         {
-            using (var session = _sessionFactory.OpenSession())
-            {
-                using (session.BeginTransaction())
-                {
-                    return session.QueryOver<Serialized.OpenTab>().List().Select(Map);
-                }
-            }
+            return GetAll().Select(Map);
         }
 
         private OpenTab Map(Serialized.OpenTab openTab)
