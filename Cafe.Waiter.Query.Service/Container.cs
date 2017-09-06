ba48b034d0cff8fbd0fd6072b4df097cb1fd0627
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-09-06 20:42:03 +0100

    Add Container singleton to Cafe.Waiter.Query.Service

---
+++ Cafe.Waiter.Query.Service/Container.cs
@@ -0,0 +1,14 @@
+﻿using Castle.Windsor;
+
+namespace Cafe.Waiter.Query.Service
+{
+    public static class Container
+    {
+        static Container()
+        {
+            Instance = new WindsorContainer();
+        }
+
+        public static IWindsorContainer Instance { get; }
+    }
+}
