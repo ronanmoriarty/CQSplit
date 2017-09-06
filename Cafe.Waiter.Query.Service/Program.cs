7e1eacc2a456c94e9eec3007e9cd92777854536c
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-09-06 22:34:38 +0100

    Bootstrap and start WaiterQueryService in Program.Main()

--- Cafe.Waiter.Query.Service/Program.cs
+++ Cafe.Waiter.Query.Service/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Cafe.Waiter.Query.Service
+﻿using Topshelf;
+
+namespace Cafe.Waiter.Query.Service
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Bootstrapper.Start();
+
+            HostFactory.Run(x =>
+            {
+                x.Service<WaiterQueryService>(waiterQueryService =>
+                {
+                    waiterQueryService.ConstructUsing(Container.Instance.Resolve<WaiterQueryService>);
+                    waiterQueryService.WhenStarted(tc => tc.Start());
+                    waiterQueryService.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDisplayName("CQRSTutorial Waiter Query Service");
+                x.SetServiceName("cqrstutorial-waiter-query-service");
+                x.SetDescription("Service to update waiter read model");
+            });
         }
     }
 }
