2ecde80cc5a53700a5da87091d62d1319ab58c27
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-09-06 13:13:13 +0100

    Update queue names to reflect command type names

--- Cafe.Waiter.Service/Messaging/MessageBusEndpointConfiguration.cs
+++ Cafe.Waiter.Service/Messaging/MessageBusEndpointConfiguration.cs
@@ -10,11 +10,11 @@ namespace Cafe.Waiter.Service.Messaging
             ReceiveEndpoints = new List<ReceiveEndpointMapping>
             {
                 // TODO Will have a thnk about what kind of tests I want around these mappings. Not sure at this stage.
-                new ReceiveEndpointMapping("open_tab", typeof(OpenTabConsumer)),
-                new ReceiveEndpointMapping("close_tab", typeof(CloseTabConsumer)),
-                new ReceiveEndpointMapping("mark_drinks_served", typeof(MarkDrinksServedConsumer)),
-                new ReceiveEndpointMapping("mark_food_served", typeof(MarkFoodServedConsumer)),
-                new ReceiveEndpointMapping("place_order", typeof(PlaceOrderConsumer)),
+                new ReceiveEndpointMapping("open_tab_command", typeof(OpenTabConsumer)),
+                new ReceiveEndpointMapping("close_tab_command", typeof(CloseTabConsumer)),
+                new ReceiveEndpointMapping("mark_drinks_served_command", typeof(MarkDrinksServedConsumer)),
+                new ReceiveEndpointMapping("mark_food_served_command", typeof(MarkFoodServedConsumer)),
+                new ReceiveEndpointMapping("place_order_command", typeof(PlaceOrderConsumer)),
             };
         }
 
