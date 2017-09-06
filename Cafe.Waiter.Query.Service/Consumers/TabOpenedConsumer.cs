78ef50eb533245b3321c0b1f7a90be42e0c852da
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-09-06 22:45:11 +0100

    Add TODO to remove Cafe.Domain reference from Cafe.Waiter.Query.Service

--- Cafe.Waiter.Query.Service/Consumers/TabOpenedConsumer.cs
+++ Cafe.Waiter.Query.Service/Consumers/TabOpenedConsumer.cs
@@ -2,7 +2,7 @@ using Cafe.Domain.Events;
 
 namespace Cafe.Waiter.Query.Service.Consumers
 {
-    public class TabOpenedConsumer : Consumer<TabOpened>
+    public class TabOpenedConsumer : Consumer<TabOpened> // TODO remove reference to Cafe.Domain - reference constraint on IConsumer is preventing use of ITabOpened instead.
     {
     }
 }
