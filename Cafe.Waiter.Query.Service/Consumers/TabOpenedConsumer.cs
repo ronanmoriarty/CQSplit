5eff52bc1696f2c4ddba574981f446731da97fd9
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-09-08 09:10:16 +0100

    Remove fixed TODO around breaking dependency from query service to domain project

--- Cafe.Waiter.Query.Service/Consumers/TabOpenedConsumer.cs
+++ Cafe.Waiter.Query.Service/Consumers/TabOpenedConsumer.cs
@@ -3,7 +3,7 @@ using Cafe.Waiter.Query.Service.Projectors;
 
 namespace Cafe.Waiter.Query.Service.Consumers
 {
-    public class TabOpenedConsumer : Consumer<TabOpened> // TODO remove reference to Cafe.Domain - reference constraint on IConsumer is preventing use of ITabOpened instead.
+    public class TabOpenedConsumer : Consumer<TabOpened>
     {
         public TabOpenedConsumer(ITabOpenedProjector projector)
             : base(projector)
