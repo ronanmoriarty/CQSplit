6a576b64dd308a6fad9c15cb271d3b437191c5f1
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-09-17 12:27:09 +0100

    Push menu options down into DetailsController

---
+++ Cafe.Waiter.Web/App/Controllers/DetailsController.js
@@ -0,0 +1,20 @@
+﻿waiterModule.controller('DetailsController', ['$scope', function($scope) {
+        $scope.options = [{
+                id: 123,
+                text: 'Bacon & Cheese Burger'
+            }, {
+                id: 124,
+                text: 'Steak & Chips'
+            }, {
+                id: 234,
+                text: 'Coca Cola'
+            }, {
+                id: 235,
+                text: 'Fanta'
+            }, {
+                id: 236,
+                text: 'Lemonade'
+            }
+        ];
+    }
+]);
