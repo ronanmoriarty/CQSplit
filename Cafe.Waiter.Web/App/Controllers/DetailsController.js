1aab3dcf5ea3f4dbc07d35958d43efca4c76d22e
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-10-15 15:09:28 +0100

    Fix function to return / throw from all paths

--- Cafe.Waiter.Web/App/Controllers/DetailsController.js
+++ Cafe.Waiter.Web/App/Controllers/DetailsController.js
@@ -83,16 +83,17 @@
             });
         }
 
-        function getQueryVariable(variable) {
+        function getQueryVariable(key) {
             var query = window.location.search.substring(1);
             var vars = query.split("&");
             for (var i = 0; i < vars.length; i++) {
                 var pair = vars[i].split("=");
-                if (decodeURIComponent(pair[0]) === variable) {
+                if (decodeURIComponent(pair[0]) === key) {
                     return decodeURIComponent(pair[1]);
                 }
             }
-            console.log("Query variable %s not found", variable);
+
+            throw ("QueryString key %s not found", key);
         }
 
         function getStatus(statusId) {
