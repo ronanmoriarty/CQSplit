0fd9d429f10dc74e39f88f38fa8eb5aba7287ced
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-10-25 15:33:17 +0100

    Add SqlConnectionFactory

---
+++ CQRSTutorial.DAL/SqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace CQRSTutorial.DAL
+{
+    public class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly IConnectionStringProviderFactory _connectionStringProviderFactory;
+
+        public SqlConnectionFactory(IConnectionStringProviderFactory connectionStringProviderFactory)
+        {
+            _connectionStringProviderFactory = connectionStringProviderFactory;
+        }
+
+        public SqlConnection Create()
+        {
+            return new SqlConnection(_connectionStringProviderFactory.GetConnectionStringProvider().GetConnectionString());
+        }
+    }
+}
