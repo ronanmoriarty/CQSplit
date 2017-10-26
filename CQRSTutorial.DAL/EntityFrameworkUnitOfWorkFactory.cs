b720c648a49a8d005ded333fcedc10a19014502f
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-10-26 23:13:35 +0100

    Add EntityFrameworkUnitOfWorkFactory

---
+++ CQRSTutorial.DAL/EntityFrameworkUnitOfWorkFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace CQRSTutorial.DAL
+{
+    public class EntityFrameworkUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private readonly IConnectionStringProviderFactory _connectionStringProviderFactory;
+
+        public EntityFrameworkUnitOfWorkFactory(IConnectionStringProviderFactory connectionStringProviderFactory)
+        {
+            _connectionStringProviderFactory = connectionStringProviderFactory;
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new EntityFrameworkUnitOfWork(_connectionStringProviderFactory);
+        }
+    }
+}
