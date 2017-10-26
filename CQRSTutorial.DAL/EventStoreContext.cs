d6542b6fa5c989674eaff47188f86ef0d370d7a3
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-10-26 23:11:41 +0100

    Add EventStoreContext

---
+++ CQRSTutorial.DAL/EventStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace CQRSTutorial.DAL
+{
+    public class EventStoreContext : DbContext
+    {
+        public EventStoreContext(string connectionString)
+            : base(new SqlConnection(connectionString), true)
+        {
+        }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventToPublish> EventsToPublish { get; set; }
+    }
+}
