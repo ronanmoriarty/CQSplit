3615a567d6a9a31b21217cb621d20ca498d5a4a3
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-11-01 09:27:52 +0000

    Remove IMapToTable interface (was only required for NHibernate configuration)

--- Cafe.Waiter.Queries.DAL/Serialized/OpenTab.cs
+++ Cafe.Waiter.Queries.DAL/Serialized/OpenTab.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using CQRSTutorial.DAL;
 
 namespace Cafe.Waiter.Queries.DAL.Serialized
 {
-    public class OpenTab : IMapToTable
+    public class OpenTab
     {
         public virtual Guid Id { get; set; }
         public virtual string Data { get; set; }
