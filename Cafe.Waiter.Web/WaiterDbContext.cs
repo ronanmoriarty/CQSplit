415cfc865295f05e2fa79a09b6e3881ac446704c
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-08 22:39:48 +0000

    Add copy of WaiterDbContext to Cafe.Waiter.Web and change OpenTabsRepository in Cafe.Waiter.Web to use it

---
+++ Cafe.Waiter.Web/WaiterDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Cafe.Waiter.Queries.DAL.Serialized;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cafe.Waiter.Web
+{
+    public class WaiterDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public WaiterDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<OpenTab> OpenTabs { get; set; }
+        public DbSet<TabDetails> TabDetails { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
