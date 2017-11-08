58252b6d419c87d2d9e102fa5d5048ce93b1d40f
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-11-08 10:29:55 +0000

    Remove unused overload of RegisterAllConsumerTypes and make RegisterConsumerType() private

--- CQRSTutorial.Messaging/ConsumerRegistrar.cs
+++ CQRSTutorial.Messaging/ConsumerRegistrar.cs
@@ -14,18 +14,9 @@ namespace CQRSTutorial.Messaging
             _consumerTypeProvider = consumerTypeProvider;
         }
 
-        public void RegisterConsumerType(IReceiveEndpointConfigurator endpointConfigurator, Type consumerType)
+        private void RegisterConsumerType(IReceiveEndpointConfigurator endpointConfigurator, Type consumerType)
         {
             endpointConfigurator.Consumer(consumerType, _consumerFactory.Create);
-        }
-
-        public void RegisterAllConsumerTypes<TBusFactoryConfigurator>(TBusFactoryConfigurator sbc, Action<TBusFactoryConfigurator, Type> action)
-            where TBusFactoryConfigurator : IBusFactoryConfigurator
-        {
-            foreach (var consumerType in _consumerTypeProvider.GetConsumerTypes())
-            {
-                action(sbc, consumerType);
-            }
         }
 
         public void RegisterAllConsumerTypes<TBusFactoryConfigurator>(TBusFactoryConfigurator sbc, Action<TBusFactoryConfigurator, Action<IReceiveEndpointConfigurator>> action) where TBusFactoryConfigurator : IBusFactoryConfigurator
--- CQRSTutorial.Messaging/IConsumerRegistrar.cs
+++ CQRSTutorial.Messaging/IConsumerRegistrar.cs
@@ -5,8 +5,6 @@ namespace CQRSTutorial.Messaging
 {
     public interface IConsumerRegistrar
     {
-        void RegisterConsumerType(IReceiveEndpointConfigurator endpointConfigurator, Type consumerType);
-        void RegisterAllConsumerTypes<TBusFactoryConfigurator>(TBusFactoryConfigurator sbc, Action<TBusFactoryConfigurator, Type> action) where TBusFactoryConfigurator : IBusFactoryConfigurator;
         void RegisterAllConsumerTypes<TBusFactoryConfigurator>(TBusFactoryConfigurator sbc, Action<TBusFactoryConfigurator, Action<IReceiveEndpointConfigurator>> action) where TBusFactoryConfigurator : IBusFactoryConfigurator;
     }
 }
