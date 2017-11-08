2b2244d70d097d7238184a587166fd732b59fe01
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-08 22:32:11 +0000

    Add .NET Framework version of WaiterDbContext to Cafe.Waiter.EventProjecting.Service

---
+++ Cafe.Waiter.EventProjecting.Service/WaiterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.SqlClient;
+using Cafe.Waiter.Queries.DAL.Serialized;
+
+namespace Cafe.Waiter.EventProjecting.Service
+{
+    public class WaiterDbContext : DbContext
+    {
+        public WaiterDbContext(string connectionString)
+            : base(new SqlConnection(connectionString), true)
+        {
+        }
+
+        public DbSet<OpenTab> OpenTabs { get; set; }
+        public DbSet<TabDetails> TabDetails { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
