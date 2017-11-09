01a154404f7798586618298d9bf5d7910f4e81d0
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-11-09 09:09:53 +0000

    Create copy of WaiterDbContextAdapter that uses Cafe.Waiter.Web.WaiterDbContext (relying on EntityFrameworkCore)

---
+++ Cafe.Waiter.Web/WaiterDbContextAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Cafe.Waiter.Queries.DAL;
+using Cafe.Waiter.Queries.DAL.Serialized;
+using Cafe.Waiter.Web.Repositories;
+using CQRSTutorial.DAL.Common;
+
+namespace Cafe.Waiter.Web
+{
+    public class WaiterDbContextAdapter : IWaiterDbContext
+    {
+        private readonly IConnectionStringProvider _connectionStringProvider;
+
+        public WaiterDbContextAdapter(IConnectionStringProvider connectionStringProvider)
+        {
+            _connectionStringProvider = connectionStringProvider;
+        }
+
+        public IQueryable<Menu> Menus => GetWaiterDbContext().Menus;
+
+        public IQueryable<OpenTab> OpenTabs => GetWaiterDbContext().OpenTabs;
+
+        public IQueryable<TabDetails> TabDetails => GetWaiterDbContext().TabDetails;
+
+        public void AddOpenTab(OpenTab openTab)
+        {
+            var waiterDbContext = GetWaiterDbContext();
+            waiterDbContext.OpenTabs.Add(openTab);
+            waiterDbContext.SaveChanges();
+        }
+
+        private WaiterDbContext GetWaiterDbContext()
+        {
+            return new WaiterDbContext(_connectionStringProvider.GetConnectionString());
+        }
+    }
+}
