84c474566eb5f60f9398a7b72aada2512720edfc
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-10 00:45:45 +0000

    Change OpenTabInserter to take IConnectionStringProvider instead of IWaiterDbContext

--- Cafe.Waiter.EventProjecting.Service/DAL/OpenTabInserter.cs
+++ Cafe.Waiter.EventProjecting.Service/DAL/OpenTabInserter.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Linq;
-using Cafe.Waiter.Queries.DAL;
 using Cafe.Waiter.Queries.DAL.Models;
+using CQRSTutorial.DAL.Common;
 using Newtonsoft.Json;
 
 namespace Cafe.Waiter.EventProjecting.Service.DAL
 {
     public class OpenTabInserter : IOpenTabInserter
     {
-        private readonly IWaiterDbContext _waiterDbContext;
+        private readonly IConnectionStringProvider _connectionStringProvider;
 
-        public OpenTabInserter(IWaiterDbContext waiterDbContext)
+        public OpenTabInserter(IConnectionStringProvider connectionStringProvider)
         {
-            _waiterDbContext = waiterDbContext;
+            _connectionStringProvider = connectionStringProvider;
         }
 
         public void Insert(OpenTab openTab)
@@ -20,17 +20,24 @@ namespace Cafe.Waiter.EventProjecting.Service.DAL
             var existingOpenTab = Get(openTab.Id);
             if (existingOpenTab == null)
             {
-                _waiterDbContext.AddOpenTab(new Queries.DAL.Serialized.OpenTab
+                var waiterDbContext = GetWaiterDbContext();
+                waiterDbContext.OpenTabs.Add(new Queries.DAL.Serialized.OpenTab
                 {
                     Id = openTab.Id,
                     Data = JsonConvert.SerializeObject(openTab)
                 });
+                waiterDbContext.SaveChanges();
             }
         }
 
         private Queries.DAL.Serialized.OpenTab Get(Guid id)
         {
-            return _waiterDbContext.OpenTabs.SingleOrDefault(x => x.Id == id);
+            return GetWaiterDbContext().OpenTabs.SingleOrDefault(x => x.Id == id);
+        }
+
+        private WaiterDbContext GetWaiterDbContext()
+        {
+            return new WaiterDbContext(_connectionStringProvider.GetConnectionString());
         }
     }
 }
