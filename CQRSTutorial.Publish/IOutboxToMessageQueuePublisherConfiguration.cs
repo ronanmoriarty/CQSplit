c99a23af5e8ed8d1257a72b4a10e65e1edc562ad
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-11 20:21:27 +0000

    Remove redundant properties from IOutboxToMessageQueuePublisherConfiguration

--- CQRSTutorial.Publish/IOutboxToMessageQueuePublisherConfiguration.cs
+++ CQRSTutorial.Publish/IOutboxToMessageQueuePublisherConfiguration.cs
@@ -3,8 +3,5 @@
     public interface IOutboxToMessageQueuePublisherConfiguration
     {
         int BatchSize { get; }
-        string QueueName { get; }
-        string QueryToWatch { get; }
-        string ServiceName { get; }
     }
 }
--- Cafe.Waiter.Command.Service/OutboxToMessageQueuePublisherConfiguration.cs
+++ Cafe.Waiter.Command.Service/OutboxToMessageQueuePublisherConfiguration.cs
@@ -7,8 +7,5 @@ namespace Cafe.Waiter.Command.Service
     public class OutboxToMessageQueuePublisherConfiguration : IOutboxToMessageQueuePublisherConfiguration
     {
         public int BatchSize => Convert.ToInt32(ConfigurationManager.AppSettings["BatchSize"]);
-        public string QueueName => ConfigurationManager.AppSettings["QueueName"];
-        public string QueryToWatch => ConfigurationManager.AppSettings["QueryToWatch"];
-        public string ServiceName => ConfigurationManager.AppSettings["ServiceName"];
     }
 }
