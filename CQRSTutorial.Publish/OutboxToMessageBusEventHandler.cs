c364edb0ce3944c1bdd744949a3a40e80fd08c6c
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-14 23:46:08 +0000

    Add OutboxToMessageBusEventHandler to wrap around OutboxToMessageBusPublisher

---
+++ CQRSTutorial.Publish/OutboxToMessageBusEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CQRSTutorial.Core;
+
+namespace CQRSTutorial.Publish
+{
+    public class OutboxToMessageBusEventHandler : IEventHandler
+    {
+        private readonly IOutboxToMessageBusPublisher _outboxToMessageBusPublisher;
+
+        public OutboxToMessageBusEventHandler(IOutboxToMessageBusPublisher outboxToMessageBusPublisher)
+        {
+            _outboxToMessageBusPublisher = outboxToMessageBusPublisher;
+        }
+
+        public void Handle(IEnumerable<IEvent> events)
+        {
+            _outboxToMessageBusPublisher.PublishEvents(events);
+        }
+    }
+}
