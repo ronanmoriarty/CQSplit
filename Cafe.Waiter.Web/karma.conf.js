82b5eea39ec2fb68481ef1fbbedfff765221f371
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-19 23:08:55 +0000

    Load module, controller and ngRoute

--- Cafe.Waiter.Web/karma.conf.js
+++ Cafe.Waiter.Web/karma.conf.js
@@ -16,6 +16,9 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/angular/angular.js',
+      'node_modules/angular-route/angular-route.js',
+      'wwwroot/App/WaiterModule.js',
+      'wwwroot/App/**/*Controller.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'wwwroot/App/**/*spec.js'
     ],
