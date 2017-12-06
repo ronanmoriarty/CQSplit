19de5795f3ec66696f4298a56d8cffa27f9eea5e
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-12-06 15:13:44 +0000

    Test that we post to /api/tab/create when creating a new tab

--- Cafe.Waiter.Web/wwwroot/App/Controllers/TabController.spec.js
+++ Cafe.Waiter.Web/wwwroot/App/Controllers/TabController.spec.js
@@ -3,7 +3,10 @@ describe('TabController', function() {
         ctrl,
         tabs,
         tab1,
-        tab2;
+        tab2,
+        $httpBackend,
+        waiter,
+        tableNumber;
 
     beforeEach(function() {
         module('waiter');
@@ -26,15 +29,36 @@ describe('TabController', function() {
             tab1,
             tab2
         ];
+        tableNumber = 11;
+        waiter = 'David';
         $scope = $rootScope.$new();
         _$httpBackend_
             .when('GET', '/api/tab')
             .respond(200, tabs);
         ctrl = $controller('TabController', {$scope: $scope});
         _$httpBackend_.flush();
+        $httpBackend = _$httpBackend_;
     }));
 
     it('should load tabs from api', function() {
         assert.equal($scope.tabs.length, 2);
+    });
+
+    describe('when tabs loaded', function() {
+        beforeEach(function() {
+            $scope.waiter = waiter;
+            $scope.formData = {
+                tableNumber: tableNumber
+            };
+        });
+
+        describe('when creating new tab', function() {
+            it('should post to api', function() {
+                $httpBackend.expectPOST('/api/tab/create', {waiter: waiter, tableNumber: tableNumber}).respond(200, '');
+
+                $scope.createNewTab();
+                $httpBackend.flush();
+            });
+        });
     });
 });
