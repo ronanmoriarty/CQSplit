48c10ffb1247685643e320d8a85369afc8d27376
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-12-06 16:56:52 +0000

    Tidy up TabController.js

--- Cafe.Waiter.Web/wwwroot/App/Controllers/TabController.js
+++ Cafe.Waiter.Web/wwwroot/App/Controllers/TabController.js
@@ -1,37 +1,40 @@
 ﻿waiterModule.controller("TabController", ["$scope", "$http", "$location", "notificationService", function ($scope, $http, $location, notificationService) {
-    var tabListUrl = "/api/tab";
     $scope.waiter = "Jim"; // hard-coded for now. TODO get this from api later
     $http({
         method: "GET",
-        url: tabListUrl
+        url: getUrlToListTabs()
     }).then(function (successResponse) {
-        $scope.tabs = successResponse.data.map(function(tab) {
-            return {
-                tableNumber: tab.tableNumber,
-                waiter: tab.waiter,
-                status: tab.status,
-                id: tab.id
-            };
-        });
+        $scope.tabs = successResponse.data;
     }, function (errorResponse) {
         console.log(errorResponse);
     });
 
-    var createTabUrl = "/api/tab/create";
+    function getUrlToListTabs(){
+        return '/api/tab';
+    }
+
     $scope.createNewTab = function() {
         $http({
             method: "POST",
-            url: createTabUrl,
-            data: {
-                waiter: $scope.waiter,
-                tableNumber: $scope.formData.tableNumber
-            }
+            url: getUrlToCreateTab(),
+            data: getDataToCreateNewTab()
         }).then(function (successResponse) {
             notificationService.success('Tab created.');
         }, function (errorResponse) {
             notificationService.error('An error occurred while creating new tab.');
         });
     };
+
+    function getUrlToCreateTab(){
+        return "/api/tab/create";
+    }
+
+    function getDataToCreateNewTab(){
+        return {
+            waiter: $scope.waiter,
+            tableNumber: $scope.formData.tableNumber
+        };
+    }
 
     // $scope.viewDetails = function(id) {
     //     $location.path("/details").search({tabId: id});
