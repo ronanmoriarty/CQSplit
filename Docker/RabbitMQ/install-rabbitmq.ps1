cdee31b922442e05a3c3f476563c44dd7e4ccce5
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-02-22 09:00:15 +0000

    Remove redundant scripts

--- Docker/RabbitMQ/install-rabbitmq.ps1
+++
@@ -1,3 +0,0 @@
-choco install rabbitmq -y
-[Environment]::SetEnvironmentVariable("PATH", [Environment]::GetEnvironmentVariable("PATH") + ";C:\Program Files\RabbitMQ Server\rabbitmq_server-3.6.14\sbin\", "Machine")
-rabbitmq-plugins enable rabbitmq_management
