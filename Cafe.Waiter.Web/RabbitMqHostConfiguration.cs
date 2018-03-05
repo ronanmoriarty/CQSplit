b3c5c5bf638afb661033931f08edb39e8ac1e8e0
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-03-05 13:40:33 +0000

    Add new RabbitMqHostConfiguration to allow RabbitMq IP address to be changed in hosts file

---
+++ Cafe.Waiter.Web/RabbitMqHostConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CQRSTutorial.Messaging.RabbitMq;
+using Microsoft.Extensions.Configuration;
+
+namespace Cafe.Waiter.Web
+{
+    public class RabbitMqHostConfiguration : IRabbitMqHostConfiguration
+    {
+        private readonly IConfigurationSection _configurationSection;
+
+        public RabbitMqHostConfiguration(IConfigurationRoot configuration)
+        {
+            _configurationSection = configuration.GetSection("RabbitMq");
+        }
+
+        public string Uri => _configurationSection["Uri"];
+        public string Username => _configurationSection["Username"];
+        public string Password => _configurationSection["Password"];
+    }
+}
