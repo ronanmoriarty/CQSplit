d092372618ac97a78ea08a0a14bae48322f45836
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-03-14 15:26:37 +0000

    Remove redundant setup files as we're moving to Docker

--- setup.ps1
+++
@@ -1,3 +0,0 @@
-[Environment]::SetEnvironmentVariable("RABBITMQ_URI", "rabbitmq://localhost", "Machine")
-[Environment]::SetEnvironmentVariable("RABBITMQ_USERNAME", "guest", "Machine")
-[Environment]::SetEnvironmentVariable("RABBITMQ_PASSWORD", "guest", "Machine")
