5cda1566e6a09410f0e48a44e2886caf40833b99
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-03-28 16:57:56 +0100

    Create database if it doesn't exist
    Create database if it doesn't exist

--- src/PowerShell/setupDatabase.ps1
+++ src/PowerShell/setupDatabase.ps1
@@ -1,19 +1,43 @@
+[CmdletBinding()]
+Param(
+    [string]$dbName = 'CQRSTutorial.Cafe.Waiter.WriteModel',
+    [string]$dbFolder = 'C:\app\databases'
+)
+
 [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
 
 $server = new-object ("Microsoft.SqlServer.Management.Smo.Server") "."
-$dbName = 'CQRSTutorial.Cafe.Waiter.WriteModel'
-
-if($server.Databases[$dbName] -eq $null)
+$database = $server.Databases[$dbName]
+if($database -eq $null)
 {
     Write-Host "$dbName database not found."
-    $mdfFilePath = ".\databases\$dbName.mdf"
+    $mdfFilePath = "$dbFolder\$dbName.mdf"
     if(Test-Path $mdfFilePath)
     {
         Write-Host "WriteModel database $mdfFilePath found. Attaching as $dbName..."
     }
     else
     {
-        Write-Host "$mdfFilePath does not exist. Creating new database..."
+        if(!(Test-Path $dbFolder))
+        {
+            mkdir $dbFolder
+        }
+
+        try
+        {
+            Write-Host "$mdfFilePath does not exist. Creating new database..."
+            $database = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -ArgumentList $server, $dbName
+            $database.DatabaseOptions.AutoShrink = $true
+            $primaryFileGroup = New-Object -TypeName Microsoft.SqlServer.Management.Smo.FileGroup -ArgumentList $database, 'PRIMARY'
+            $database.FileGroups.Add($primaryFileGroup)
+            $dataFile = New-Object -TypeName Microsoft.SqlServer.Management.Smo.DataFile -ArgumentList $primaryFileGroup, "$($dbName)_Data", $mdfFilePath
+            $primaryFileGroup.Files.Add($dataFile)
+            $database.Create()
+        }
+        catch [Exception]
+        {
+            Write-Output $_.Exception|format-list -force
+        }
     }
 }
 else
