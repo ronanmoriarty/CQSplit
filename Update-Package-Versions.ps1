ba509f2a8a90487a15711acf423059a29dc89440
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-30 17:33:05 +0100

    Inspect current version

--- Update-Package-Versions.ps1
+++ Update-Package-Versions.ps1
@@ -1,3 +1,13 @@
-Get-ChildItem -Path .\ -Filter *.nuspec -Recurse | % {
-    Write-Output $_.FullName
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$VersionNumber
+)
+Get-ChildItem -Path .\src\CQRS\ -Filter *.nuspec -Recurse | ForEach-Object {
+    $xml = New-Object -TypeName System.Xml.XmlDocument
+    $xml.Load($_.FullName)
+    $ns = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xml.NameTable
+    $ns.AddNamespace("ns", $xml.DocumentElement.NamespaceURI)
+    $versionNode = $xml.SelectSingleNode('//ns:version', $ns)
+    Write-Output "$($_.FullName) has version number $($versionNode.InnerXml)"
 }
