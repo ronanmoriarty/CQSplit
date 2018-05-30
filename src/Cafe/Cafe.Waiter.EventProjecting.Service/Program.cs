c5e240d273b114228b00a3074b8fbd39ce457886
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-30 12:00:13 +0100

    Remove TopShelf config in startup for both services

--- src/Cafe/Cafe.Waiter.EventProjecting.Service/Program.cs
+++ src/Cafe/Cafe.Waiter.EventProjecting.Service/Program.cs
@@ -1,27 +1,19 @@
-﻿using Topshelf;
-
-namespace Cafe.Waiter.EventProjecting.Service
+﻿namespace Cafe.Waiter.EventProjecting.Service
 {
     class Program
     {
         static void Main(string[] args)
         {
             Bootstrapper.Start();
-
-            HostFactory.Run(x =>
+            var service = Container.Instance.Resolve<EventProjectingService>();
+            try
             {
-                x.Service<EventProjectingService>(eventProjectingService =>
-                {
-                    eventProjectingService.ConstructUsing(Container.Instance.Resolve<EventProjectingService>);
-                    eventProjectingService.WhenStarted(tc => tc.Start());
-                    eventProjectingService.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsLocalSystem();
-
-                x.SetDisplayName("CQRSTutorial Waiter Event Projecting Service");
-                x.SetServiceName("cqrstutorial-waiter-event-projecting-service");
-                x.SetDescription("Service to project waiter events into the waiter read model");
-            });
+                service.Start();
+            }
+            finally
+            {
+                service.Stop();
+            }
         }
     }
 }
