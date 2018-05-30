580e24d280a0eb0f20d214e92707c05b914ff6e2
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-30 12:53:22 +0100

    Update DAL projects to use .NET Core 2.0

--- src/CQRS/CQRSTutorial.DAL/EventStoreDbContext.cs
+++ src/CQRS/CQRSTutorial.DAL/EventStoreDbContext.cs
@@ -1,14 +1,20 @@
-﻿using System.Data.Entity;
-using System.Data.SqlClient;
-using CQRSTutorial.DAL.Serialized;
+﻿using CQRSTutorial.DAL.Serialized;
+using Microsoft.EntityFrameworkCore;
 
 namespace CQRSTutorial.DAL
 {
     public class EventStoreDbContext : DbContext
     {
+        private readonly string _connectionString;
+
         public EventStoreDbContext(string connectionString)
-            : base(new SqlConnection(connectionString), true)
         {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
         }
 
         public DbSet<Event> Events { get; set; }
