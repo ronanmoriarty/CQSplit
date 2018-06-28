26d168c633c85ffc4f2282f4dfaa3e7bcc3a9cf0
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-06-28 14:14:57 +0100

    Implement OpenTabs.DeleteTabsFor()

--- src/Cafe/Cafe.Waiter.AcceptanceTests/OpenTabs.cs
+++ src/Cafe/Cafe.Waiter.AcceptanceTests/OpenTabs.cs
@@ -1,9 +1,16 @@
-﻿namespace Cafe.Waiter.AcceptanceTests
+﻿using CQRSTutorial.DAL.Common;
+using CQRSTutorial.DAL.Tests.Common;
+
+namespace Cafe.Waiter.AcceptanceTests
 {
     public class OpenTabs
     {
         public static void DeleteTabsFor(string waiter)
         {
+            var connectionStringProvider = EnvironmentVariableConnectionStringProviderFactory.Get("CQRSTUTORIAL_CAFE_WAITER_READMODEL_CONNECTIONSTRING_OVERRIDE");
+            var sqlExecutor = new SqlExecutor(new ConnectionStringOverride(connectionStringProvider.GetConnectionString()));
+            var commandText = $"DELETE FROM dbo.OpenTabs WHERE [Data] LIKE '%\"Waiter\":\"{waiter}\"%'";
+            sqlExecutor.ExecuteNonQuery(commandText);
         }
 
         public ContainsSingleTabConstraintBuilder ContainsSingleTab { get; } = new ContainsSingleTabConstraintBuilder();
