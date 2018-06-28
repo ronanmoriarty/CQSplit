f0f7ba3d38c9e4887d461695c60c877e23b8026b
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-06-28 09:51:59 +0100

    Create empty Cafe.Waiter.AcceptanceTests project

---
+++ src/Cafe/Cafe.Waiter.AcceptanceTests/TabAcceptanceTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Cafe.Waiter.AcceptanceTests
+{
+    [TestFixture]
+    public class TabAcceptanceTest
+    {
+        [Test]
+        public void Created_tab_is_displayed_on_open_tabs_view()
+        {
+        }
+    }
+}
