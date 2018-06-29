b021ba85107bfb61a6d45b06d68fd1fd589c1a05
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-06-29 09:38:05 +0100

    Add logging to diagnose why log messages for consumer registration are occurring twice

--- src/Cafe/Cafe.Waiter.Command.Service/Bootstrapper.cs
+++ src/Cafe/Cafe.Waiter.Command.Service/Bootstrapper.cs
@@ -8,8 +8,11 @@ namespace Cafe.Waiter.Command.Service
 {
     public class Bootstrapper
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Bootstrapper));
+
         public static void Start()
         {
+            Logger.Debug("Bootstrapping...");
             var loggerRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(loggerRepository, new FileInfo("log4net.config"));
             Container.Instance.Install(FromAssembly.Instance(Assembly.GetExecutingAssembly()));
