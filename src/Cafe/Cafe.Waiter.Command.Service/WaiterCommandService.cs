ab1310470b2ebd24466ea42fa5079bd890b11005
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-06-29 10:24:54 +0100

    Avoid creating bus control (and registering consumers) twice

--- src/Cafe/Cafe.Waiter.Command.Service/WaiterCommandService.cs
+++ src/Cafe/Cafe.Waiter.Command.Service/WaiterCommandService.cs
@@ -1,25 +1,21 @@
-﻿using CQRSTutorial.Messaging;
-using CQRSTutorial.Publish;
+﻿using CQRSTutorial.Publish;
 using MassTransit;
 
 namespace Cafe.Waiter.Command.Service
 {
     public class WaiterCommandService
     {
-        private readonly IMessageBusFactory _messageBusFactory;
         private readonly IOutboxToMessageBusPublisher _outboxToMessageBusPublisher;
-        private IBusControl _busControl;
+        private readonly IBusControl _busControl;
 
-        public WaiterCommandService(IMessageBusFactory messageBusFactory,
-            IOutboxToMessageBusPublisher outboxToMessageBusPublisher)
+        public WaiterCommandService(IBusControl busControl, IOutboxToMessageBusPublisher outboxToMessageBusPublisher)
         {
-            _messageBusFactory = messageBusFactory;
             _outboxToMessageBusPublisher = outboxToMessageBusPublisher;
+            _busControl = busControl;
         }
 
         public void Start()
         {
-            _busControl = _messageBusFactory.Create();
             _busControl.Start();
             _outboxToMessageBusPublisher.PublishQueuedMessages();
         }
