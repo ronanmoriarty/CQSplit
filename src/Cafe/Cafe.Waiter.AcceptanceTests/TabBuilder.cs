1e0b409b1a038624b808053a894a10055bb0f969
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-06-29 17:58:36 +0100

    Enter table number and submit

--- src/Cafe/Cafe.Waiter.AcceptanceTests/TabBuilder.cs
+++ src/Cafe/Cafe.Waiter.AcceptanceTests/TabBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Cafe.Waiter.AcceptanceTests
 {
@@ -23,6 +25,16 @@ namespace Cafe.Waiter.AcceptanceTests
         public void WaitingAMaximumOf(TimeSpan timeSpan)
         {
             _timeSpan = timeSpan;
+            var chromeDriver = new ChromeDriver
+            {
+                Url = "http://localhost:5000/app/index.html#!/tabs"
+            };
+            var tableNumberTextBox = chromeDriver.FindElementById("tableNumber");
+            tableNumberTextBox.SendKeys(_tableNumber.ToString());
+            var createTabButton = chromeDriver.FindElement(By.XPath("//input[@type=\"submit\"]"));
+            createTabButton.Click();
+            chromeDriver.FindElementByXPath($"//td[text() = \"{_waiter}\"]");
+            chromeDriver.Close();
         }
     }
 }
