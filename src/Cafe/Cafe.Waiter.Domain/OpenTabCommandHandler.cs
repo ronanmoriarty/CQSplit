d4d762a0d9d0fe9302786485c5e26fa798f3833f
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-07-18 14:46:58 +0100

    Add logging to show when TabOpened events are being created.

--- src/Cafe/Cafe.Waiter.Domain/OpenTabCommandHandler.cs
+++ src/Cafe/Cafe.Waiter.Domain/OpenTabCommandHandler.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using Cafe.Waiter.Contracts.Commands;
 using Cafe.Waiter.Events;
 using CQRSTutorial.Core;
+using log4net;
 
 namespace Cafe.Waiter.Domain
 {
     public class OpenTabCommandHandler : ICommandHandler<IOpenTabCommand>
     {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(OpenTabCommandHandler));
+
         public IEnumerable<IEvent> Handle(IOpenTabCommand command)
         {
+            var eventId = Guid.NewGuid();
+            _logger.Debug($"Creating event {eventId} for command {command.Id} on aggregate {command.AggregateId}");
             return new IEvent[]
             {
                 new TabOpened
                 {
-                    Id = Guid.NewGuid(),
+                    Id = eventId,
                     CommandId = command.Id,
                     AggregateId = command.AggregateId,
                     TableNumber = command.TableNumber,
