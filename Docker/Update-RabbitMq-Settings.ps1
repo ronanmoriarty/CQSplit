517471c60d8fbca744852558198a5e78f9019fbe
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-07-20 17:22:26 +0100

    Create array of 3 files to create

--- Docker/Update-RabbitMq-Settings.ps1
+++ Docker/Update-RabbitMq-Settings.ps1
@@ -1,3 +1,13 @@
 $rabbitMqContainerId = docker container list --filter ancestor=rabbitmq --format "{{.ID}}"
 $rabbitMqServerIpAddress = docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $rabbitMqContainerId
 Write-Output "`$rabbitMqServerIpAddress:$rabbitMqServerIpAddress"
+
+$appSettingsFiles = @(
+    "..\src\Cafe\Cafe.Waiter.Web\appsettings.override.json",
+    "..\src\Cafe\Cafe.Waiter.Command.Service\appsettings.override.json",
+    "..\src\Cafe\Cafe.Waiter.EventProjecting.Service\appsettings.override.json"
+)
+
+$appSettingsFiles | ForEach-Object {
+    Write-Host $_
+}
