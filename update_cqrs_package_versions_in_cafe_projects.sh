5ebe1e8c672dde311c713fc3ff6691731f5a2d8a
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-01 10:29:16 +0100

    Remove update_cqrs_package_versions_in_cafe_projects.sh in favour of new powershell script

--- update_cqrs_package_versions_in_cafe_projects.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" -o -z "$2" ]; then echo "Expected syntax: ./update_cqrs_package_versions_in_cafe_projects [old_version_number] [new_version_number]"; exit; fi
-
-find ./src/Cafe/ -name '*csproj' -exec sed -i "s/$1/$2/g" {} \;
