310a80e80f7fe35bb052231fad26e3d46523492c
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-01 09:56:34 +0100

    Add powershell version of script to update CQRS package versions in Cafe projects

---
+++ Update-CQRS-Package-Versions-In-Cafe-Projects.ps1
@@ -0,0 +1,11 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$oldVersion,
+    [Parameter(Mandatory=$true)]
+    [string]$newVersion
+)
+
+Get-ChildItem -Path .\src\Cafe\ -Filter *.csproj -Recurse | ForEach-Object {
+    (Get-Content $_.FullName).Replace($oldVersion, $newVersion) | Set-Content $_.FullName
+}
