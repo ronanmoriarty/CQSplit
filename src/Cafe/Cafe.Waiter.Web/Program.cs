ed35c1873e89b016d4bdbbcb71eef951daea643e
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-01 15:05:42 +0100

    Update Cafe.Waiter.Web and tests to use NLog

--- src/Cafe/Cafe.Waiter.Web/Program.cs
+++ src/Cafe/Cafe.Waiter.Web/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System.IO;
-using System.Reflection;
-using log4net;
-using log4net.Config;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Cafe.Waiter.Web
@@ -10,9 +7,6 @@ namespace Cafe.Waiter.Web
     {
         public static void Main(string[] args)
         {
-            var loggerRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(loggerRepository, new FileInfo("log4net.config"));
-
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
