f50668888ea716dffcc4fa2938530aad7db0b41a
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-06 14:07:00 +0100

    Change default database to master (for now)

--- src/Cafe/Docker/SqlServer/ReadModel/Scripts/06 - Add EventProjectingService Login.sql
+++ src/Cafe/Docker/SqlServer/ReadModel/Scripts/06 - Add EventProjectingService Login.sql
@@ -2,7 +2,7 @@ IF NOT EXISTS(SELECT 1 FROM sys.syslogins WHERE [name] = 'EventProjectingService
 BEGIN
 	CREATE LOGIN [EventProjectingService]
 		WITH PASSWORD=$(EventProjectingServicePassword),
-		DEFAULT_DATABASE=[CQRSTutorial.Cafe.Waiter.ReadModel],
+		DEFAULT_DATABASE=[master],
 		DEFAULT_LANGUAGE=[us_english],
 		CHECK_EXPIRATION=OFF,
 		CHECK_POLICY=OFF
