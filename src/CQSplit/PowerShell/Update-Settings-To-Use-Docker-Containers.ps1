0959205c7e168f4ea67721eb8aeb68e3bc56af1a
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-14 19:52:13 +0100

    Fix function call name and args (supply empty list of paths for now)

--- src/CQSplit/PowerShell/Update-Settings-To-Use-Docker-Containers.ps1
+++ src/CQSplit/PowerShell/Update-Settings-To-Use-Docker-Containers.ps1
@@ -9,11 +9,19 @@ function GetCQSplitKeyValuePairs()
     return $keyValuePairs
 }
 
+function GetAppSettingsTemplateFiles()
+{
+    return @() # no appSettings.json.template files just yet that need IP addresses injected - this will change shortly.
+}
+
 $keyValuePairs = GetCQSplitKeyValuePairs
 
 $keyValuePairs.Keys | ForEach-Object {
     Write-Output "$($_): $($keyValuePairs[$_])"
 }
 
-$path = GetFullPath ..\
-SwapPlaceholdersInExampleFilesToCreateNewDockerJsonFiles $path appSettings.json.template appSettings.json $keyValuePairs
+$path = GetFullPath "$PSScriptRoot\..\"
+Write-Output "`$path: $path"
+$paths = GetAppSettingsTemplateFiles
+
+SwapPlaceholdersToCreateNewJsonFiles $paths appSettings.json $keyValuePairs
