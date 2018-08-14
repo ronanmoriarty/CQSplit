17ba603acc9cdd55446159f8e581aa30428a38cd
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-14 19:50:54 +0100

    Only get passwords and rabbitmq container ip address

--- src/CQSplit/PowerShell/Update-Settings-To-Use-Docker-Containers.ps1
+++ src/CQSplit/PowerShell/Update-Settings-To-Use-Docker-Containers.ps1
@@ -1,7 +1,15 @@
 . "$PSScriptRoot\Docker.ps1"
 . "$PSScriptRoot\FileOperations.ps1"
 
-$keyValuePairs = GetKeyValuePairs
+function GetCQSplitKeyValuePairs()
+{
+    $keyValuePairs = GetPasswordKeyValuePairs
+    $rabbitMqServerAddress = GetRabbitMqAddress
+    $keyValuePairs.Add("`$rabbitMqServerAddress", $rabbitMqServerAddress)
+    return $keyValuePairs
+}
+
+$keyValuePairs = GetCQSplitKeyValuePairs
 
 $keyValuePairs.Keys | ForEach-Object {
     Write-Output "$($_): $($keyValuePairs[$_])"
