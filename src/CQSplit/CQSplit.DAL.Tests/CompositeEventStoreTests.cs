bb4a415d3a1fb0a3935cef0e0b257118e223290e
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-15 11:09:19 +0100

    Add test around CompositeEventStore.Add()

---
+++ src/CQSplit/CQSplit.DAL.Tests/CompositeEventStoreTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using CQSplit.Core;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CQSplit.DAL.Tests
+{
+    [TestFixture]
+    public class CompositeEventStoreTests
+    {
+        private IEnumerable<IEventStore> _eventStores;
+        private CompositeEventStore _compositeEventStore;
+        private IEvent _event;
+        private IEventStore _eventStore1;
+        private IEventStore _eventStore2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _eventStore1 = Substitute.For<IEventStore>();
+            _eventStore2 = Substitute.For<IEventStore>();
+            _eventStores = new[] { _eventStore1, _eventStore2 };
+            _compositeEventStore = new CompositeEventStore(_eventStores);
+            _event = new TestEvent();
+        }
+
+        [Test]
+        public void Add_delegates_to_all_underlying_event_stores()
+        {
+            _compositeEventStore.Add(_event);
+
+            _eventStore1.Received(1).Add(Arg.Is(_event));
+            _eventStore2.Received(1).Add(Arg.Is(_event));
+        }
+    }
+}
---
+++ src/CQSplit/CQSplit.DAL.Tests/TestEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CQSplit.Core;
+
+namespace CQSplit.DAL.Tests
+{
+    public class TestEvent : IEvent
+    {
+        public Guid Id { get; set; }
+        public Guid AggregateId { get; set; }
+        public Guid CommandId { get; set; }
+    }
+}
