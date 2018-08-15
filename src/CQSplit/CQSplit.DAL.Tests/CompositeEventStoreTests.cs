1637d072166496bbbe82fe9a5104c7f0d3e581c6
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-15 11:13:18 +0100

    Add test around setting of unit of work

--- src/CQSplit/CQSplit.DAL.Tests/CompositeEventStoreTests.cs
+++ src/CQSplit/CQSplit.DAL.Tests/CompositeEventStoreTests.cs
@@ -13,6 +13,7 @@ namespace CQSplit.DAL.Tests
         private IEvent _event;
         private IEventStore _eventStore1;
         private IEventStore _eventStore2;
+        private IUnitOfWork _unitOfWork;
 
         [SetUp]
         public void SetUp()
@@ -22,6 +23,7 @@ namespace CQSplit.DAL.Tests
             _eventStores = new[] { _eventStore1, _eventStore2 };
             _compositeEventStore = new CompositeEventStore(_eventStores);
             _event = new TestEvent();
+            _unitOfWork = Substitute.For<IUnitOfWork>();
         }
 
         [Test]
@@ -31,6 +33,15 @@ namespace CQSplit.DAL.Tests
 
             _eventStore1.Received(1).Add(Arg.Is(_event));
             _eventStore2.Received(1).Add(Arg.Is(_event));
+        }
+
+        [Test]
+        public void Setting_unit_of_work_sets_unit_of_work_for_all_underlying_event_stores()
+        {
+            _compositeEventStore.UnitOfWork = _unitOfWork;
+
+            Assert.That(_eventStore1.UnitOfWork, Is.EqualTo(_unitOfWork));
+            Assert.That(_eventStore2.UnitOfWork, Is.EqualTo(_unitOfWork));
         }
     }
 }
