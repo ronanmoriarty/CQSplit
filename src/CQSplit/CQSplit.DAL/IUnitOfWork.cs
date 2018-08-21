bcb7f448c22078bf0ccac0a9856b803362b571fb
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-08-22 00:48:47 +0100

    Remove IUnitOfWork.Enrolling()

--- src/CQSplit/CQSplit.DAL/IUnitOfWork.cs
+++ src/CQSplit/CQSplit.DAL/IUnitOfWork.cs
@@ -5,7 +5,6 @@ namespace CQSplit.DAL
     public interface IUnitOfWork : IDisposable
     {
         void Commit();
-        IUnitOfWork Enrolling(params IHaveUnitOfWork[] haveUnitOfWorks);
         void ExecuteInTransaction(Action action);
     }
 }
