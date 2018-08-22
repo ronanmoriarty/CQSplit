72f29368db5e739ca96825ee143129a2a733da78
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-22 16:31:02 +0100

    Remove ReadModelConnectionStringProvider

--- src/Cafe/Cafe.Waiter.Queries.DAL/ReadModelConnectionStringProvider.cs
+++
@@ -1,9 +0,0 @@
-using Cafe.DAL.Common;
-
-namespace Cafe.Waiter.Queries.DAL
-{
-    public class ReadModelConnectionStringProvider
-    {
-        public static string ConnectionString = ConfigurationRoot.Instance["connectionString"];
-    }
-}
