36d0e1d1d31af0da5bed3f1a642a3b399e51f2be
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-08-22 19:30:51 +0100

    Update package pattern for publish to leave out trailing dot

--- src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
+++ src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
@@ -12,4 +12,4 @@ Write-Output "`$version: $version"
 git tag -a $version -m "Corresponds to $version nuget packages."
 git push --tags
 
-dotnet nuget push .\src\.nuget.local\CQSplit.*.$version.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
+dotnet nuget push .\src\.nuget.local\CQSplit*.$version.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
