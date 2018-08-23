6b654fa28c8fdf84c70167ecbc122897f65f8441
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-08-24 00:59:55 +0100

    Change event-projecting service's bootstrapper so that only one thread can access it at a time

--- src/Cafe/Cafe.Waiter.EventProjecting.Service/Bootstrapper.cs
+++ src/Cafe/Cafe.Waiter.EventProjecting.Service/Bootstrapper.cs
@@ -1,13 +1,26 @@
 using System.Reflection;
 using Castle.Windsor.Installer;
+using NLog;
 
 namespace Cafe.Waiter.EventProjecting.Service
 {
     public class Bootstrapper
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        private static bool _started;
+        private static readonly object LockObj = new object();
+
         public static void Start()
         {
-            Container.Instance.Install(FromAssembly.Instance(Assembly.GetExecutingAssembly()));
+            lock (LockObj)
+            {
+                if (!_started)
+                {
+                    Logger.Debug("Bootstrapping...");
+                    Container.Instance.Install(FromAssembly.Instance(Assembly.GetExecutingAssembly()));
+                    _started = true;
+                }
+            }
         }
     }
 }
