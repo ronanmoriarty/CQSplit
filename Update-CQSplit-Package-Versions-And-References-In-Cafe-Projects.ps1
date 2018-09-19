3d6e605afba204a1ec625ec90c3e915391b5c007
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-09-19 15:06:30 +0100

    Move Update-CQSplit-Package-Versions-In-Cafe-Projects.ps1 into Update-CQSplit-Package-Versions-And-References-In-Cafe-Projects.ps1 (too many scripts!)

--- Update-CQSplit-Package-Versions-And-References-In-Cafe-Projects.ps1
+++ Update-CQSplit-Package-Versions-And-References-In-Cafe-Projects.ps1
@@ -6,4 +6,17 @@ UpdateCQSplitPackageVersions
 $newVersion = GetVersion
 git tag -a $newVersion -m "Corresponds to $newVersion nuget packages."
 
-.\Update-CQSplit-Package-Versions-In-Cafe-Projects.ps1 -oldVersion $oldVersion -newVersion $newVersion
+function UpdateCQSplitPackageVersionsInCafeProjects([string]$oldVersion, [string]$newVersion)
+{
+    Get-ChildItem -Path .\src\Cafe\ -Filter *.csproj -Recurse | ForEach-Object {
+        # TODO: could do with tightening this up - on the offchance that there's some other
+        # 3rd party package with the same version number we'll end up updating that one too.
+        # Chances are that would cause a failing build though so would be easily spotted.
+        (Get-Content $_.FullName).Replace($oldVersion, $newVersion) | Set-Content $_.FullName
+    }
+
+    git add .\src\Cafe\*.csproj
+    git commit -m "Update Cafe projects to reference $newVersion CQSplit packages"
+}
+
+UpdateCQSplitPackageVersionsInCafeProjects $oldVersion $newVersion
