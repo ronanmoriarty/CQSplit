939bfe8ba1db9bd23463adad75b6f4221056aa98
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-09-19 15:33:33 +0100

    Add RabbitMqHostConfiguration classes into each service

---
+++ src/Cafe/Cafe.Waiter.Command.Service/RabbitMqHostConfiguration.cs
@@ -0,0 +1,22 @@
+using System;
+using CQSplit.Messaging.RabbitMq;
+using Microsoft.Extensions.Configuration;
+
+namespace Cafe.Waiter.Command.Service
+{
+    public class RabbitMqHostConfiguration : IRabbitMqHostConfiguration
+    {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public RabbitMqHostConfiguration(IConfigurationRoot configurationRoot)
+        {
+            _configurationRoot = configurationRoot;
+        }
+
+        public string Uri => _configurationRoot["rabbitmq:uri"];
+        public string Username => _configurationRoot["rabbitmq:username"];
+        public string Password => _configurationRoot["rabbitmq:password"];
+        public int RetryLimit => Convert.ToInt32(_configurationRoot["rabbitmq:retryLimit"]);
+        public int DelayInSecondsBetweenRetries => Convert.ToInt32(_configurationRoot["rabbitmq:delayInSecondsBetweenRetries"]);
+    }
+}
