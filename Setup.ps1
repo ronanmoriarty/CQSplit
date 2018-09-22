e292d020187e8fa70cd43b3aa5b40f27d6597fd6
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-09-22 23:23:49 +0100

    Create appSettings.docker.json files for *all* appSettings.json.template files (including ones missed in command-service-tests and event-projecting-service-tests projects)

--- Setup.ps1
+++ Setup.ps1
@@ -14,17 +14,6 @@ param (
 . .\src\CQSplit\PowerShell\Docker.ps1
 . .\src\CQSplit\PowerShell\FileOperations.ps1
 
-function GetJsonTemplateFiles()
-{
-    return @(`
-        "$PSScriptRoot\src\Cafe\Cafe.Waiter.Web\appSettings.json.template", `
-        "$PSScriptRoot\src\Cafe\Cafe.Waiter.Web.Tests\appSettings.json.template", `
-        "$PSScriptRoot\src\Cafe\Cafe.Waiter.Command.Service\appSettings.json.template", `
-        "$PSScriptRoot\src\Cafe\Cafe.Waiter.EventProjecting.Service\appSettings.json.template", `
-        "$PSScriptRoot\src\Cafe\Cafe.Waiter.Acceptance.Tests\appSettings.json.template" `
-    )
-}
-
 function CreateEnvFile()
 {
     Write-Output "sa_password=$saPasswordPlainText" | Out-File -encoding ASCII .env
@@ -42,7 +31,7 @@ $eventProjectingServicePasswordPlainText = ConvertToPlainText $eventProjectingSe
 CreateEnvFile
 $dockerKeyValuePairs = GetKeyValuePairsToUseInsideContainers
 $unitTestKeyValuePairs = GetKeyValuePairsForUnitTests
-$jsonTemplateFiles = GetJsonTemplateFiles
+$jsonTemplateFiles = GetAppSettingsTemplateFiles
 SwapPlaceholdersToCreateNewJsonFiles $jsonTemplateFiles appSettings.docker.json $dockerKeyValuePairs $IsCiBuild
 SwapPlaceholdersToCreateNewJsonFiles $jsonTemplateFiles appSettings.json $unitTestKeyValuePairs $IsCiBuild
 
