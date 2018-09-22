0247b14380e2c8c6b119ecaedb494470b87b8ff8
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-09-22 10:49:51 +0100

    Update tags locally before pushing new tags

--- src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
+++ src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
@@ -10,6 +10,7 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $version = GetVersion
 Write-Output "`$version: $version"
 
+git fetch --tags --force
 git push --tags
 
 dotnet nuget push .\src\.nuget.local\CQSplit*.$version.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
