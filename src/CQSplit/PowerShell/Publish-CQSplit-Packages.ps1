a51de8d20858fcf0b1b9982694e7d1629f7f43ec
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-09-22 12:08:08 +0100

    Ignore errors on tag-fetch and tag-push...
    Sometimes the fetch 'fails' because all the tags are up-to-date, or the push 'fails' but actually pushes the tags. Can't see a way to distinguish between a real
    failure and one of these scenarios above, so ignore the 'failures' for tag-fetch and tag-push and manually fix up afterwards if need be

--- src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
+++ src/CQSplit/PowerShell/Publish-CQSplit-Packages.ps1
@@ -10,7 +10,9 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $version = GetVersion
 Write-Output "`$version: $version"
 
+$ErrorActionPreference = 'Continue' # fetch fails if up-to-date. tag push also "fails" but pushes the tags anyway! Just ignore the lot and fix up tags manually afterwards if need be!
 git fetch --tags --force
 git push --tags
 
+$ErrorActionPreference = 'Stop'
 dotnet nuget push .\src\.nuget.local\CQSplit*.$version.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
